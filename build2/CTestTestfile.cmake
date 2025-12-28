5a2f7f83a614c5978c23fb881265cf8fd1e3cef7
khaaliswooden-max <khaaliswooden@gmail.com>
2025-12-27 21:46:07 -0600

    Phase 1

---
+++ build2/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admin/OneDrive - Visionblox/Documents/GitHub/c_plusx2_project
+# Build directory: C:/Users/Admin/OneDrive - Visionblox/Documents/GitHub/c_plusx2_project/build2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_tensor "C:/Users/Admin/OneDrive - Visionblox/Documents/GitHub/c_plusx2_project/build2/test_tensor.exe")
+set_tests_properties(test_tensor PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/OneDrive - Visionblox/Documents/GitHub/c_plusx2_project/CMakeLists.txt;71;add_test;C:/Users/Admin/OneDrive - Visionblox/Documents/GitHub/c_plusx2_project/CMakeLists.txt;0;")
